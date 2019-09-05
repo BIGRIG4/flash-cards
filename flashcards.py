18b992d639b81516371dbe783290d2f1e29e5956
Nicholas Becker <Nicholas.becker@students.dominican.edu>
2019-09-05 13:59:57 -0700

    official first commit

---
+++ flashcards.py
@@ -0,0 +1,18 @@
+    import json
+    with open('me-capitals.json', 'r') as f:
+    data = json.load(f)
+
+    total = len(data)["cards"])
+    score = 0
+
+        for i in data["cards"]:
+            guess = input(i["q"] + " > ")
+
+        if guess == i["a"]:
+            score += 1
+            print("Correct!")
+            print(print(f"Current score: {score}/{total}")
+
+    else:
+        print("Incorrect! The correct answer was", i["a"])
+        print(f"Current score: {score}/{total}")
